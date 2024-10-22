1f0d1ca4bbb0919106b3bc9d3beb1d462b69a19c
nxxtia <126991328+nxxtia@users.noreply.github.com>
2024-10-22 22:42:15 +0300

    parser created

---
+++ README.md
@@ -0,0 +1,9 @@
+### My Parser
+
+my first test parsing usage
+
+![text](image.png)
+
+### Example
+
+Ok([1, 1, 2, 3, 5, 8])
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+peg::parser! {
+    pub grammar list_parser() for str {
+      rule number() -> u32
+        = n:$(['0'..='9']+) {? n.parse().or(Err("u32")) }
+  
+      pub rule list() -> Vec<u32>
+        = "[" l:(number() ** ",") "]" { l }
     }
-}
+  }
